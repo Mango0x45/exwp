aa6aa5322b85c68e20f179cb895a513e9b9d6617
Thomas Voss <mail@thomasvoss.com>
2023-12-13 21:50:48 +0100

    Support release- and debug builds

--- base.mk
+++ base.mk
@@ -2,11 +2,15 @@ MAKEFLAGS = -j$(shell nproc)
 
 CC = cc
 CFLAGS += \
-	-Wall -Wextra -Wpedantic -Werror \
-	-D_GNU_SOURCE -pipe -O3 \
-	-march=native -mtune=native \
+	-Wall -Wextra -Wpedantic -Werror -D_GNU_SOURCE -pipe \
 	$(shell pkg-config --cflags $(lib) 2>/dev/null)
 LDLIBS += $(shell pkg-config --libs $(lib) 2>/dev/null)
+
+ifeq ($(BUILD),release)
+	CFLAGS += -O3 -march=native -mtune=native
+else
+	CFLAGS += -g -ggdb3
+endif
 
 all: $(bin)
 $(bin): $(obj)
